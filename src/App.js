7ec3555bf1eff295861414c16f917d7b1cbe838d
EsitoneX <phoom0529@gmail.com>
2021-01-15 19:39:37 +0700

    :heavy_plus_sign: Add tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="flex flex-row h-screen justify-center items-center bg-blue-300">
+            <p className="text-6xl font-bold text-blue-900">Initial Project</p>
+        </div>
+    );
+};
 
 export default App;
